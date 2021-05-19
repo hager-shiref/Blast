aaa9b4ff4d74c1dbe608796db3aaa3328d4c2e15
hager-shiref <hagershreif5@gmail.com>
2021-05-20 01:01:03 +0200

    update

--- lib/blastn/blastn.dart
+++ lib/blastn/blastn.dart
@@ -1,283 +1,282 @@
-// import 'dart:math';
-// import 'package:firebase_database/firebase_database.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:untitled/model/model.dart';
-// class Blast{
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:untitled/model/model.dart';
+class Blast{
+String blast= BlastModel['Chromosome'];
+  static double alignment_score=0;
+  //compare single base
+int SingleBaseCompare(String seq1,String seq2,int i,int j){
+  if(seq1[i] == seq2[j])return 2;
+  else return -1;
+}
+List ?SMalignment(String seq1,String seq2){
+    int m=seq1.length;
+    int n=seq2.length;
+   int g=-3;
+   List matrix=[];
+   for(int i=0;i<m;i++){
+     List tmp=[];
+     for(int j=0;j<n;j++){
+       tmp.add(0);
+     }
+     matrix.add(tmp);
+     }
+    for (int sii=0;sii<m;sii++){
+      matrix[sii][0]=sii*g;
+    }
+for (int sjj=0;sjj<n;sjj++){
+      matrix[sjj][0]=sjj*g;
+    }
+  for(int siii=0;siii<m;siii++){
+      for(int sjjj=0;sjjj<n;sjjj++){
+     matrix[siii][sjjj] =([matrix[siii-1][sjjj] + g , matrix[siii - 1][sjjj - 1] + SingleBaseCompare(seq1,seq2,siii, sjjj), matrix[siii][sjjj-1] + g]).reduce((value, element) => value>element?value:element);
+      }  
+  }
+  List sequ1 = [seq1[m-1]];
+  List  sequ2 = [seq2[n-1]];
+  while(m>1 && n>1){
+    List max1=[matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]].reduce((value, element) =>value>element?value:element );
+    List max2=[matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]].reduce((value, element) => value>element?value:element);
+    if(max1==matrix[m-2][n-2]){
+       m -= 1;
+        n -= 1;
+        sequ1.add(seq1[m-1]);
+         sequ2.add(seq2[n-1]);
+    }
+    else if(max2==matrix[m-1][n-2]){
+       n -= 1;
+       sequ1.add('-');
+      sequ2.add(seq2[n-1]);
+    }
+    else{
+       m -= 1;
+       sequ1.add(seq1[m-1]);
+      sequ2.add('-');
+    }
+    for(var i=0;i<sequ1.length/2;i++){
+        var temp = sequ1[i];
+        sequ1[i] = sequ1[sequ1.length-1-i];
+        sequ1[sequ1.length-1-i] = temp;
+        return sequ1;
+    }
+    for(var i=0;i<sequ2.length/2;i++){
+        var temp = sequ2[i];
+        sequ2[i] = sequ2[sequ2.length-1-i];
+        sequ2[sequ2.length-1-i] = temp;
+        return sequ2;
+    }
+   String align_seq1 = sequ1.join('');
+   String align_seq2=sequ2.join('') ;
+   double  align_score = .0;
+   for(int k=0;k<align_seq1.length;k++){
+     if(align_seq1[k]==align_seq2[k]){
+       align_score += 1; 
+     }
+   }
+ align_score=(align_score)/align_seq1.length;
+      alignment_score=align_score*100;
+    List aligns=[align_seq1 , align_seq2, align_score];
+ return aligns;
+  }
+}
+display(String seque1,String seque2){
+  int le=60;
+  while(seque1.length>=0){
+     Text('sequence1: ');
+     for(int a=seque1[le-40] as int;a<le;a++){
+       Text('$a');
+     }
+     Text("\n");
+        Text('           ');
+    for(int k=le-40;k<le;k++){
+       if (seque1[k] == seque2[k])  Text('|');
+            else Text(' ');
+    }
+    Text("\n");
+    Text('sequence2: ');
+    for(int b=seque2[le-40] as int;b<le;b++){
+       Text('$b');
+    }
+    Text("\n");
+     le += 40;
+     if(seque2.length>le-40){
+       Text('sequence1: ');
+       for(int a=seque2[le-40] as int;a<seque2.length;a++){
+          Text('$a');
+       }
+       Text("\n");
+        Text('           ');
+        for(int k=seque1[le-40] as int;k<seque1.length;k++){
+          if(seque1[k]==seque2[k]){
+            Text('|');
+          }
+          else{Text(' ');}
+          Text("\n");
+        Text('sequence2: ');
+        for(int b=seque2[le-40] as int;b<seque2.length;b++){
+          Text('$b');
+        }
+         Text("\n");
+        }
+     }
+  }
+}
+//transform base to numeric value
+List wordToNum(String word){
+   List h=[];
+   for(int w=0;w<word.length;w++){
+     if(word[w]=='A'){
+       h.add(1);
+     }
+     else if(word[w]=='C'){
+       h.add(2);
+     }
+     else if(word[w]=='G'){
+       h.add(3);
+     }
+     else { h.add(4);}
+   }
+ return h;
+ }
+//transform word with 11 bases to its index
+num wordToIndex(String word,int wordlen){
+  num tmp=0;
+  List wordnum=wordToNum(word); 
+  for(int i=0; i<wordnum.length;i++){
+    int v=wordnum[i];
+    num p=pow(4,(wordlen-i));
+    tmp+=(v-1)* p;
+  }  
+  return tmp;
+}
+List getWordsPosition(String word){
+   assert (word.length==11);
+   num seekIndex=wordToIndex(word,11-1);
+   List positions=<Map>[];
+  
+   for(int chrNames=0;chrNames<blast.length;chrNames++){
+
+     String chrSeq=blast;
+      String position= chrSeq.substring((seekIndex as int) ,1) ;
+      try{
+          positions.add(position[0][-1].split(','));
+      }
+      catch (e){
+          positions.add([]);
+      }
+      
+   }
+   return positions;
+  
+ }
  
-//   static double? alignment_score;
-//   //compare single base
-// int SingleBaseCompare(String seq1,String seq2,int i,int j){
-//   if(seq1[i] == seq2[j])return 2;
-//   else return -1;
-// }
-// List ?SMalignment(String seq1,String seq2){
-//     int m=seq1.length;
-//     int n=seq2.length;
-//    int g=-3;
-//    List matrix=[];
-//    for(int i=0;i<m;i++){
-//      List tmp=[];
-//      for(int j=0;j<n;j++){
-//        tmp.add(0);
-//      }
-//      matrix.add(tmp);
-//      }
-//     for (int sii=0;sii<m;sii++){
-//       matrix[sii][0]=sii*g;
-//     }
-// for (int sjj=0;sjj<n;sjj++){
-//       matrix[sjj][0]=sjj*g;
-//     }
-//   for(int siii=0;siii<m;siii++){
-//       for(int sjjj=0;sjjj<n;sjjj++){
-//      matrix[siii][sjjj] =([matrix[siii-1][sjjj] + g , matrix[siii - 1][sjjj - 1] + SingleBaseCompare(seq1,seq2,siii, sjjj), matrix[siii][sjjj-1] + g]).reduce((value, element) => value>element?value:element);
-//       }  
-//   }
-//   List sequ1 = [seq1[m-1]];
-//   List  sequ2 = [seq2[n-1]];
-//   while(m>1 && n>1){
-//     List max1=[matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]].reduce((value, element) =>value>element?value:element );
-//     List max2=[matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]].reduce((value, element) => value>element?value:element);
-//     if(max1==matrix[m-2][n-2]){
-//        m -= 1;
-//         n -= 1;
-//         sequ1.add(seq1[m-1]);
-//          sequ2.add(seq2[n-1]);
-//     }
-//     else if(max2==matrix[m-1][n-2]){
-//        n -= 1;
-//        sequ1.add('-');
-//       sequ2.add(seq2[n-1]);
-//     }
-//     else{
-//        m -= 1;
-//        sequ1.add(seq1[m-1]);
-//       sequ2.add('-');
-//     }
-//     for(var i=0;i<sequ1.length/2;i++){
-//         var temp = sequ1[i];
-//         sequ1[i] = sequ1[sequ1.length-1-i];
-//         sequ1[sequ1.length-1-i] = temp;
-//         return sequ1;
-//     }
-//     for(var i=0;i<sequ2.length/2;i++){
-//         var temp = sequ2[i];
-//         sequ2[i] = sequ2[sequ2.length-1-i];
-//         sequ2[sequ2.length-1-i] = temp;
-//         return sequ2;
-//     }
-//    String align_seq1 = sequ1.join('');
-//    String align_seq2=sequ2.join('') ;
-//    double  align_score = .0;
-//    for(int k=0;k<align_seq1.length;k++){
-//      if(align_seq1[k]==align_seq2[k]){
-//        align_score += 1; 
-//      }
-//    }
-//  align_score=(align_score)/align_seq1.length;
-//       alignment_score=align_score;
-//     List aligns=[align_seq1 , align_seq2, align_score];
-//  return aligns;
-//   }
-// }
-// display(List seque1,List seque2){
-//   int le=60;
-//   while(seque1.length>=0){
-//      Text('sequence1: ');
-//      for(int a=seque1[le-40];a<le;a++){
-//        Text('$a');
-//      }
-//      Text("\n");
-//         Text('           ');
-//     for(int k=le-40;k<le;k++){
-//        if (seque1[k] == seque2[k])  print('|');
-//             else Text(' ');
-//     }
-//     Text("\n");
-//     Text('sequence2: ');
-//     for(int b=seque2[le-40];b<le;b++){
-//        Text('$b');
-//     }
-//     Text("\n");
-//      le += 40;
-//      if(seque2.length>le-40){
-//        Text('sequence1: ');
-//        for(int a=seque2[le-40];a<seque2.length;a++){
-//           Text('$a');
-//        }
-//        Text("\n");
-//         Text('           ');
-//         for(int k=seque1[le-40];k<seque1.length;k++){
-//           if(seque1[k]==seque2[k]){
-//             Text('|');
-//           }
-//           else{Text(' ');}
-//           Text("\n");
-//         Text('sequence2: ');
-//         for(int b=seque2[le-40];b<seque2.length;b++){
-//           Text('$b');
-//         }
-//          Text("\n");
-//         }
-//      }
-//   }
-// }
-// //transform base to numeric value
-// List wordToNum(String word){
-//    List h=[];
-//    for(int w=0;w<word.length;w++){
-//      if(word[w]=='A'){
-//        h.add(1);
-//      }
-//      else if(word[w]=='C'){
-//        h.add(2);
-//      }
-//      else if(word[w]=='G'){
-//        h.add(3);
-//      }
-//      else { h.add(4);}
-//    }
-//  return h;
-//  }
-// //transform word with 11 bases to its index
-// num wordToIndex(String word,int wordlen){
-//   num tmp=0;
-//   List wordnum=wordToNum(word); 
-//   for(int i=0; i<wordnum.length;i++){
-//     int v=wordnum[i];
-//     num p=pow(4,(wordlen-i));
-//     tmp+=(v-1)* p;
-//   }  
-//   return tmp;
-// }
-// List getWordsPosition(String word){
-//    assert (word.length==11);
-//    num seekIndex=wordToIndex(word,11-1);
-//    List positions=<Map>[];
-  
-//    for(int chrNames=0;chrNames<retrevedData.toString().length;chrNames++){
-
-//      String chrSeq=retrevedData.toString();
-//       String position= chrSeq.substring((seekIndex as int) ,1) ;
-//       try{
-//           positions.add(position[0][-1].split(','));
-//       }
-//       catch (e){
-//           positions.add([]);
-//       }
-      
-//    }
-//    return positions;
-  
-//  }
-//  int length=retrevedData.toString().length;
-//  String extractSeq(int chrIndex,int pos,int lenght)
-//  {
-//     pos=pos+(pos/60).floor();
-//      String chr=retrevedData.toString();
-//     return chr.toString().replaceAll('\n','');
+ String extractSeq(int chrIndex,int pos,int lenght)
+ {
+    pos=pos+(pos/60).floor();
+     String chr=blast;
+    return chr.toString().replaceAll('\n','');
     
-//  }
-//   int countOccurrencesUsingLoop(List list, String element) {
-//            if (list == null || list.isEmpty) {
-//              return 0;
-//             }
-//           int count = 0;
-//           for (int i = 0; i < list.length; i++) {
-//           if (list[i] == element) {
-//             count++;
-//            }
-//          }
-//          return count;
-//         }
-//  blast(String querySeq){
-//     int i=0;
-//     List query_words=[];
-//     int querySeqLenght=querySeq.length;
-//     int wordsLenght=querySeqLenght-11+1;
-//     while (i<wordsLenght)
-//     {
-//         query_words.add(querySeq[i][i+11]);
-//         i+=1; 
-//     }
-//     List words_position=[];
-//     for ( String word in query_words){
-//       words_position.add(getWordsPosition(word));
-//     }
-//     for(int chr_index =0;chr_index<24;chr_index++){
-//       for (int word_index=0; word_index<wordsLenght;word_index++){
-//         for(int pos=0;pos>words_position[word_index][chr_index];pos++){
-//           words_position[word_index][chr_index][pos] +=wordsLenght-word_index -1;
-//         }
-//          List words_position_correct =[];
-//         while(word_index<wordsLenght){
-//           words_position_correct +=words_position[word_index][chr_index];
-//         }
-//       int A=  countOccurrencesUsingLoop(words_position_correct, 'A');
-//       int G=  countOccurrencesUsingLoop(words_position_correct, 'G');
-//       int T=  countOccurrencesUsingLoop(words_position_correct, 'T');
-//       int C=  countOccurrencesUsingLoop(words_position_correct, 'C');
-//        List words_positions_corrects_count=[{
-//           "A":A,
-//           "G":G,
-//           "C":C,
-//           "T":T
-//         }];
-//         List finded_positions=[];
-//         for (int count=0;count<words_positions_corrects_count.length;count++){
-//           if (words_positions_corrects_count[count]>5){
-//             finded_positions.add(count);
-//           }
-//           if (finded_positions !=null){
-//               for(int finded_position=0;finded_position<finded_positions.length;finded_position++){
-//                 int candidate_seq_pos = finded_position - querySeq.length + 11 - 5;
-//                 int candidate_seq_length = querySeq.length + 11;
-//                 dynamic candidate_sequence = extractSeq(chr_index,candidate_seq_pos,candidate_seq_length);
-//                  List  i_start_indexs = [];
-//                  for (int i_start=0;i_start<15;i_start++)
-//                  {
-//                    var score = SMalignment(candidate_sequence,querySeq);
-//                     i_start_indexs.add(score![0]);
-//                      i_start_indexs.add(score[1]);
-//                       i_start_indexs.add(score[2]);
+ }
+  int countOccurrencesUsingLoop(List list, String element) {
+           if (list == null || list.isEmpty) {
+             return 0;
+            }
+          int count = 0;
+          for (int i = 0; i < list.length; i++) {
+          if (list[i] == element) {
+            count++;
+           }
+         }
+         return count;
+        }
+ blastAlgorithm(String querySeq){
+    int i=0;
+    List query_words=[];
+    int querySeqLenght=querySeq.length;
+    int wordsLenght=querySeqLenght-11+1;
+    while (i<wordsLenght)
+    {
+        query_words.add(querySeq[i][i+11]);
+        i+=1; 
+    }
+    List words_position=[];
+    for ( String word in query_words){
+      words_position.add(getWordsPosition(word));
+    }
+    for(int chr_index =0;chr_index<24;chr_index++){
+      for (int word_index=0; word_index<wordsLenght;word_index++){
+        for(int pos=0;pos>words_position[word_index][chr_index];pos++){
+          words_position[word_index][chr_index][pos] +=wordsLenght-word_index -1;
+        }
+         List words_position_correct =[];
+        while(word_index<wordsLenght){
+          words_position_correct +=words_position[word_index][chr_index];
+        }
+      int A=  countOccurrencesUsingLoop(words_position_correct, 'A');
+      int G=  countOccurrencesUsingLoop(words_position_correct, 'G');
+      int T=  countOccurrencesUsingLoop(words_position_correct, 'T');
+      int C=  countOccurrencesUsingLoop(words_position_correct, 'C');
+       List words_positions_corrects_count=[{
+          "A":A,
+          "G":G,
+          "C":C,
+          "T":T
+        }];
+        List finded_positions=[];
+        for (int count=0;count<words_positions_corrects_count.length;count++){
+          if (words_positions_corrects_count[count]>5){
+            finded_positions.add(count);
+          }
+          if (finded_positions !=null){
+              for(int finded_position=0;finded_position<finded_positions.length;finded_position++){
+                int candidate_seq_pos = finded_position - querySeq.length + 11 - 5;
+                int candidate_seq_length = querySeq.length + 11;
+                dynamic candidate_sequence = extractSeq(chr_index,candidate_seq_pos,candidate_seq_length);
+                 List  i_start_indexs = [];
+                 for (int i_start=0;i_start<15;i_start++)
+                 {
+                   var score = SMalignment(candidate_sequence,querySeq);
+                    i_start_indexs.add(score![0]);
+                     i_start_indexs.add(score[1]);
+                      i_start_indexs.add(score[2]);
                       
-//                  }
-//                  var largest_start = i_start_indexs[0];
-//                       int  i_start=0;
-//                     for (var i = 0; i < i_start_indexs.length; i++) {
+                 }
+                 var largest_start = i_start_indexs[0];
+                      int  i_start=0;
+                    for (var i = 0; i < i_start_indexs.length; i++) {
       
-//                       if (i_start_indexs[i] > largest_start) {
-//                          largest_start = i_start_indexs[i];
-//                              i_start=i;
-//                        }
-//                      }
-//                     List i_end_indexs = [];
-//                     for(int i_end=1;i<16;i++){
-//                       var score=SMalignment(candidate_sequence,querySeq);
-//                        i_end_indexs.add(score);
-//                     }
-//                       var largest_end = i_end_indexs[0];
-//                       int  i_end=0;
-//                     for (var i = 0; i < i_end_indexs.length; i++) {
+                      if (i_start_indexs[i] > largest_start) {
+                         largest_start = i_start_indexs[i];
+                             i_start=i;
+                       }
+                     }
+                    List i_end_indexs = [];
+                    for(int i_end=1;i<16;i++){
+                      var score=SMalignment(candidate_sequence,querySeq);
+                       i_end_indexs.add(score);
+                    }
+                      var largest_end = i_end_indexs[0];
+                      int  i_end=0;
+                    for (var i = 0; i < i_end_indexs.length; i++) {
       
-//                       if (i_end_indexs[i] > largest_end) {
-//                          largest_end = i_end_indexs[i];
-//                              i_end=i+1;
-//                        }
-//                      } 
-//                       candidate_sequence= candidate_sequence.sublist(i_start , -i_end);
-//                      List <dynamic>  align_seq = SMalignment(candidate_sequence,querySeq)!;
+                      if (i_end_indexs[i] > largest_end) {
+                         largest_end = i_end_indexs[i];
+                             i_end=i+1;
+                       }
+                     } 
+                      candidate_sequence= candidate_sequence.sublist(i_start , -i_end);
+                     List <dynamic>  align_seq = SMalignment(candidate_sequence,querySeq)!;
 
-//                      return display(align_seq[0], align_seq[1]);
+                    Text( '${display(align_seq[0], align_seq[1])}');
 
-//               }
+              }
 
-//           }
-//         }
-//       }
-//     }
+          }
+        }
+      }
+    }
     
-//  }
+ }
 
-// }
+}
  
--- lib/src/ui/output.dart
+++ lib/src/ui/output.dart
@@ -1,123 +1,88 @@
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
-import 'package:provider/provider.dart';
-//var retrevedData=" ";
-/*var dataa;
-final db=FirebaseDatabase.instance;
-   final ref=db.reference().child('data');
-     final data=ref.child('description').once().then((DataSnapshot data){
-         retrevedData=data.value;
-         dataa=data;
-       });*/
+import 'package:untitled/blastn/blastn.dart';
+import 'package:untitled/model/model.dart';
 class Output extends StatefulWidget {
-     TextEditingController query = TextEditingController();
+  TextEditingController query = TextEditingController();
     Output(this.query);
   @override
   _OutputState createState() => _OutputState();
 }
-class _OutputState extends State<Output> {
 
+class _OutputState extends State<Output> {
+  Blast _blast=Blast();
+  double? m=0;
+  double match=Blast.alignment_score;
+  
   @override
   Widget build(BuildContext context) {
-     //Blast b=Blast();
-     //double? m=Blast.alignment_score;
-     double  match=0;
-   // if(m !=null)   match=m*100;
     return Scaffold(
+      backgroundColor: Color(0xff4a707a),
+      appBar: AppBar(
         backgroundColor: Color(0xff4a707a),
-        appBar: AppBar(
-          backgroundColor: Color(0xff4a707a),
-          elevation: 0,
-          centerTitle: true,
-          title: Text(
-            'BLASTn',
-            style: TextStyle(color: Colors.white),
-          ),
-          leading: IconButton(
-            icon: Icon(
-              Icons.chevron_left,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'BLASTn',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(child:Text('',style: TextStyle(color: Colors.white),
-         ),));
-    //     body:  FutureBuilder(
-    //       future: dataa,
-    //   builder: (BuildContext context ,AsyncSnapshot snapshot ){
-    //     if(snapshot.hasError)
-    //     {
-    //       print(snapshot.error);
-    //     }
-    //      if(snapshot.connectionState==ConnectionState.none||snapshot.connectionState==ConnectionState.waiting){
-    //             print("data has not been fetched yet");
-    //             return Center(child: CircularProgressIndicator());
-    //         }
-    //       else{
-    //         if(snapshot.hasData){
-    //           print("Data found, You can continue");
-    //           return Container(
-    //       height: MediaQuery.of(context).size.height,
-    //       width: MediaQuery.of(context).size.width,
-    //       child: Column(
-    //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //         children: [
-    //           SizedBox(
-    //             width: 250,
-    //             height: 250,
-    //             child: PieChart(
-    //               dataMap: {"Miss match": 100-match, "Match":match },
-    //               colorList: [Colors.yellow, Colors.red],
-    //               chartType: ChartType.ring,
-    //               chartValuesOptions: ChartValuesOptions(
-    //                 showChartValuesOutside: false,
-    //                 showChartValues: true,
-    //               ),
-    //             ),
-    //           ),
-    //           Padding(
-    //             padding: const EdgeInsets.all(10.0),
-    //             child: Container(
-    //               height: 170,
-    //               width: MediaQuery.of(context).size.width,
-    //               decoration: BoxDecoration(
-    //                   color: Colors.white,
-    //                   borderRadius: BorderRadius.all(Radius.circular(5))),
-    //               padding: EdgeInsets.all(10),
-    //               child: ListView.builder(
-    //                 itemCount: widget.query.toString().length.compareTo(0),
-    //                 itemBuilder: (ctx,index){
-    //                 return Row(
-    //                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //                     children: [
-    //                       Text(
-    //                         '${retrevedData}',
-    //                         style: TextStyle(
-    //                             color: Colors.black, fontWeight: FontWeight.bold),
-    //                       ),
-                          
-    //                     ],
-    //                 );
-    //               }
-    //               )
-                  
-    //             )
-    //           )
-    //         ],
-    //       ),
-    //     );
-    //         }
-    //         else{
-    //              print("No data found");
-    //              return Text("No data found");
-    //          }
-    //       }
-    //   }
-    // )
-  }}
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 250,
+              height: 250,
+              child: PieChart(
+                dataMap: {"Miss match": 100-match, "Match":match },
+                colorList: [Colors.yellow, Colors.red],
+                chartType: ChartType.ring,
+                chartValuesOptions: ChartValuesOptions(
+                  showChartValuesOutside: false,
+                  showChartValues: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 170,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          '${_blast.blastAlgorithm(widget.query.toString())}',
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
